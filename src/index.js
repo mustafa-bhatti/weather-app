55f20fb091f2c98b127dee3bb026447fe17bcf49
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-02 17:48:27 +0500

    feat: add render function to render object from forecastArray

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import './styles.css';
-import { renderWeather } from './render';
+import { renderWeather, renderForcast } from './render';
 import { fetchData, processData, forecastData } from './weather-api';
 
 const form = document.querySelector('#search-form');
@@ -15,18 +15,21 @@ form.addEventListener('submit', async function (event) {
   const processedData = processData(data);
   console.log(processedData);
   renderWeather(processedData);
+  const forArray = getForcaseData(data);
+  renderForcast(forArray);
 });
 //TODO: Implement Rendring of weather Report
 const data = await fetchData('Lahore');
 const processedData = processData(data);
 renderWeather(processedData);
 
-const getForcaseData = function () {
+const getForcaseData = function (data) {
   const forcastArray = [];
   for (let i = 1; i < 6; i++) {
     const currentDateForecast = forecastData(data.days[i]);
     forcastArray.push(currentDateForecast);
   }
-  console.log(forcastArray);
+  return forcastArray;
 };
-getForcaseData();
+const forArray = getForcaseData(data);
+renderForcast(forArray);
--- src/render.js
+++ src/render.js
@@ -1,19 +1,53 @@
-
+/* eslint-disable no-unused-vars */
 const location = document.querySelector('.location');
 const desc = document.querySelector('.desc');
 const icon = document.querySelector('.icon');
 const temp = document.querySelector('.temp');
 const min = document.querySelector('.min');
 const max = document.querySelector('.max');
+const mainCard = document.querySelector('.card');
+const forcastDaysDiv = document.querySelector('.forecastDays');
 
-export const renderWeather = function (weatherObject) {
+const renderWeather = function (weatherObject) {
   console.log(weatherObject.location);
   location.textContent = weatherObject.location;
   desc.textContent = weatherObject.desc;
-  temp.textContent = weatherObject.temp
-  min.textContent = weatherObject.min
-  max.textContent = weatherObject.max
-  import (`./images/${weatherObject.icon}.png`).then(response => {
-    icon.src = response.default
-  })
+  temp.textContent = weatherObject.temp;
+  min.textContent = weatherObject.min;
+  max.textContent = weatherObject.max;
+  import(`./images/${weatherObject.icon}.png`).then((response) => {
+    icon.src = response.default;
+  });
 };
+const renderForcast = function (forcastArray) {
+  forcastDaysDiv.innerHTML = '';
+  for (let key of forcastArray) {
+    const day = key.day;
+    const icon = key.icon;
+    const temp = key.temp;
+    const dayCard = createForcastCard(day, icon, temp);
+    forcastDaysDiv.appendChild(dayCard);
+  }
+};
+const createForcastCard = function (day, icon, temp) {
+  const cardDiv = document.createElement('div');
+  cardDiv.className = 'day-card';
+
+  const dateP = document.createElement('p');
+  dateP.textContent = day;
+
+  const iconImg = document.createElement('img');
+  import(`./images/${icon}.png`).then((response) => {
+    iconImg.src = response.default;
+  });
+  iconImg.className = 'icon';
+
+  const tempP = document.createElement('p');
+  tempP.textContent = `${temp}°`;
+
+  cardDiv.appendChild(dateP);
+  cardDiv.appendChild(iconImg);
+  cardDiv.appendChild(tempP);
+  return cardDiv;
+};
+export { renderWeather, renderForcast };
